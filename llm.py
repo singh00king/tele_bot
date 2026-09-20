fe5d4550d07b59b834dc37ad26a4961e7ec50173
Akshay <Akshaysingh19481@outlook.com>
2026-09-20 12:52:56 +0530

    testing

--- llm.py
+++ llm.py
@@ -1,14 +1,11 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-groq_key = os.getenv("API_KEY_3")
-#
+
 from langchain.chat_models import init_chat_model
 
 #Groq
 groq_llm = init_chat_model(
     model="openai/gpt-oss-20b",
     model_provider="groq",
-    api_key=groq_key
 )
-groq_llm.invoke("What is a cat?")
