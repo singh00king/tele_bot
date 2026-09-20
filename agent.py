8872ca6af5ee3faab6332bc955f9d00fa5f84d9c
Akshay <Akshaysingh19481@outlook.com>
2026-09-20 13:48:42 +0530

    added checkpointer

--- agent.py
+++ agent.py
@@ -1,9 +1,14 @@
 from llm import groq_llm
 from tools import live_cricket_score
+
 from langchain.agents import create_agent
-#
+from langgraph.checkpoint.memory import InMemorySaver
+
+memory = InMemorySaver()
+
 agent = create_agent(
     model=groq_llm,
     tools=[live_cricket_score],
-    system_prompt="You are a helpful assistant",
+    checkpointer=memory,      # ← This is the important part
+    system_prompt="You are a helpful assistant."
 )
